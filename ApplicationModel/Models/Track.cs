b2b6cabdc39e76ccc1abb9959e465505f90ace1a
Monja <monja.nya@gmail.com>
2021-02-04 14:45:28 +0200

    Scaffold track controller using in-memory database

---
+++ ApplicationModel/Models/Track.cs
@@ -0,0 +1,9 @@
+﻿namespace ApplicationModel.Models
+{
+    public class Track
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string FilePath { get; set; }
+    }
+}
---
+++ WebApplication/DataAccess/MusicContext.cs
@@ -0,0 +1,20 @@
+﻿using ApplicationModel.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication.DataAccess
+{
+    public class MusicContext : DbContext
+    {
+        public MusicContext(DbContextOptions<MusicContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Track> Tracks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Track>()
+                .ToTable("Track");
+        }
+    }
+}
